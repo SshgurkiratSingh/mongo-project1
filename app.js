7f990cd855454ea4502cd1d1ba5bf1a953ca1241
sshgurkiratsingh <appzone7092@gmail.com>
2023-01-15 14:15:39 +0530

    added home page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app=express();
 const mongoose=require('mongoose');
 require('dotenv/config');
 const postRoute=require('./post');
+
 // app.use(".025")
 app.use('/',postRoute);
 app.use(bodyParser.json());
--- post.js
+++ post.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const Post=require('./models/Post')
 const router = express.Router();
+
+  
 router.get('/home',(req,res)=>{
-    res.send("hiii");
+    router.use(express.static('public'));
+    res.sendFile(__dirname+"/public/index.html");
+    res.sendFile(__dirname+"/public/index.css");
 })
 router.post('/admi',(req,res)=>{
    console.log(req.body)
