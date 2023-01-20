310d2c581c148b5e3add6f8ab6fd0de93f078d98
istcproject <112457214+istcproject@users.noreply.github.com>
2023-01-20 12:54:59 +0530

    hi

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express=require('express');
 const post = require('./post');
-const bodyParser=require('body-parser')
+const bodyParser=require('body-parser');
 const app=express();
 const mongoose=require('mongoose');
 require('dotenv/config');
 const postRoute=require('./post');
-
+app.use(express.json());
 // app.use(".025")
 app.use('/',postRoute);
 app.use(bodyParser.json());
+mongoose.set('strictQuery', true);
 
 // 
 mongoose.connect(process.env.DB_CONNECTION,()=>console.log('connected to db'))
-
-
-
+app.post('/nnn',(req,res)=>{
+    console.log(req)
+ });
+require('./models/Post.js');
 app.use((req,res)=>{res.status(404).json({
     "reply":"not-found",
     "error":404
--- post.js
+++ post.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { ConnectionStates } = require('mongoose');
 const Post=require('./models/Post')
 const router = express.Router();
 
@@ -9,6 +10,18 @@ router.get('/home',(req,res)=>{
     res.sendFile(__dirname+"/public/index.css");
 })
 router.post('/admi',(req,res)=>{
-   console.log(req.body)
+   //console.log(req.body)
+   const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+   })
+   post.save().then(data => {
+    res.json(data);
+    console.log(data);
+   }).catch(err =>{
+    console.log(err);
+    
+    res.status(200)
+   });
 })
 module.exports=router;
